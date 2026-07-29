a34247cfe83bef12557225b289e4563b38264c91
zhea-n0x <adamfrdsid@gmail.com>
2026-07-29 15:54:39 +0700

    chore(root): allowing CORS from localhost

--- todos-be/src/main/java/com/todo_apps/todos/controller/TodoController.java
+++ todos-be/src/main/java/com/todo_apps/todos/controller/TodoController.java
@@ -21,6 +21,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:3000"})
 @RequestMapping("/api/todos")
 public class TodoController {
     private final TodoService todoService;
